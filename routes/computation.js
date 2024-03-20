783d51cb39bb446fffe7024ce1a0680d9bd73fee
prabhukumar <prabhukumara2001@gmail.com>
2024-03-20 15:49:52 -0500

    bonus done

--- routes/computation.js
+++ routes/computation.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const a = Math.random();
-const b = Math.random();
+
 router.get('/', function(req, res) {
-    const difference = Math.abs(a - b);
-    res.send(`Math.abs(a - b) applied to ${a} and ${b} is ${difference}`);
+    
+    var a = Math.random()*10+1;
+    var b = Math.random()*10+1; 
+    const absvalue = Math.abs(a - b);
+    a = Math.min(Math.max(a, -1), 1);
+    const acosresult = Math.acos(a);
+ 
+    const sinresult = Math.sin(a)
+    res.send(`Math.abs(a - b) applied to ${a} and ${b} is ${absvalue}<br>
+    Math.acos(a) applied to ${a} is ${acosresult} <br>
+    Math.sin(a) applied to ${a} is ${sinresult} <br>
+    `);
+ 
 });
+ 
+// module.exports = router;
+ 
 router.get('/', function(req, res) {
     res.send('Respond with a resource');
 });
